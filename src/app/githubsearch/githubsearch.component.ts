5c538dd50145cdfd73bfcae6da8fad0173b6640c
AlainDevis <devisalain04@gmail.com>
2020-11-15 12:11:13 +0200

    Create the 1st Component

---
+++ src/app/githubsearch/githubsearch.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-githubsearch',
+  templateUrl: './githubsearch.component.html',
+  styleUrls: ['./githubsearch.component.css']
+})
+export class GithubsearchComponent implements OnInit {
+
+  user:string;
+
+  getUser(){
+    
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
